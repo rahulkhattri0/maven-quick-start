3cf8d5a9a4ff0b64ef0d35d9a254a978bf7278ec
unknown <rahulkhattri14@gmail.com>
2022-04-10 20:38:48 +0530

    added dummy files

---
+++ src/main/java/com/rahul/demo/first.java
@@ -0,0 +1,14 @@
+package com.rahul.demo;
+
+public class first {
+    
+    public first() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	first app = new first();
+    }
+}
